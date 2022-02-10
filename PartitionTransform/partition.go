3ceb83148edfe3f359a6b58ad5939fbb0afa4845
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-02-10 15:23:14 -0300

    Partition example

--- PartitionTransform/partition.go
+++ PartitionTransform/partition.go
@@ -14,7 +14,6 @@ import (
 func init() {
 	beam.RegisterFunction(FilterAndMultiply)
 	beam.RegisterFunction(SelectNumber)
-	beam.RegisterFunction(decileFn)
 }
 
 func main() {
@@ -29,48 +28,19 @@ func main() {
 	scope := pipeline.Root()
 
 	// Creating a PCollection
-	pcollection_numbers := beam.Create(scope, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	//pcollection_text := beam.Create(scope, []string{"Hello", "World", "from", "Apache", "Beam"})
-	//scope.Scope("teste")
-
-	// Print the PCollection
-	beam.ParDo0(scope, func(numbers []int) {
-		fmt.Println(numbers)
-	}, pcollection_numbers)
-
-	// do a simple transformation on the data.
-	// pcollection_numbers = beam.ParDo(scope, func(numbers []int) []int {
-	// 	numbers[1] = 10
-
-	// 	return numbers
-	// }, pcollection_numbers)
-
-	/* Take each element of the input PCollection and continue with only the
-	elements that are even and then multiply the resulting elements by 10 */
-	pcollection_numbers = beam.ParDo(scope, FilterAndMultiply, pcollection_numbers)
-
-	// Print the PCollection
-	beam.ParDo0(scope, func(numbers []int) {
-		fmt.Println(numbers)
-	}, pcollection_numbers)
-
-	// picoles := beam.Partition(scope, 2, SelectNumber, pcollection_numbers)
-
-	students := beam.CreateList(scope, []Student{{42}, {57}, {23}, {89}, {99}, {5}, {8}, {10}, {11}, {15}, {18}})
+	pcollection_numbers := beam.CreateList(scope, []int{1, 2, 3, 4, 5, 6, 7, 8, 9})
 	// Partition returns a slice of PCollections
-	studentsByPercentile := beam.Partition(scope, 40, decileFn, students)
+	numbers_partition := beam.Partition(scope, 2, SelectNumber, pcollection_numbers)
 
-	// beam.ParDo0(scope, func(test Student) {
-	// 	fmt.Println(test)
-	// }, students)
+	// Print the []PCollection
+	// for i, v := range numbers_partition {
+	// 	PrintParDo(scope, v, i)
+	// }
 
-	for _, v := range studentsByPercentile {
-		PrintPicole(scope, v)
-	}
-
-	// avg := applyPartition(s, input)
-	// passert.Equals(s, avg, Student{42})
-	// ptest.RunAndValidate(t, p)
+	// Print the PCollection
+	beam.ParDo0(scope, func(numbers int) {
+		fmt.Println(numbers)
+	}, numbers_partition[1])
 
 	// "Run" invokes beam.Run with the runner supplied by the flag "runner".
 	if err := beamx.Run(context.Background(), pipeline); err != nil {
@@ -79,29 +49,10 @@ func main() {
 
 }
 
-func PrintPicole(scope beam.Scope, picole beam.PCollection) {
-	beam.ParDo0(scope, func(test Student) {
-		fmt.Println(test)
-	}, picole)
-}
-
-type Student struct {
-	Percentile int
-}
-
-func decileFn(student Student) int {
-	return int(float64(student.Percentile) / float64(10))
-}
-
-// applyPartition returns the 40th percentile of students.
-func ApplyPartition(s beam.Scope, students beam.PCollection) beam.PCollection {
-	// [START model_multiple_pcollections_partition]
-	// Partition returns a slice of PCollections
-	studentsByPercentile := beam.Partition(s, 10, decileFn, students)
-	// Each partition can be extracted by indexing into the slice.
-	fortiethPercentile := studentsByPercentile[4]
-	// [END model_multiple_pcollections_partition]
-	return fortiethPercentile
+func PrintParDo(scope beam.Scope, pcol beam.PCollection, index int) {
+	beam.ParDo0(scope, func(test int) {
+		fmt.Println(index, test)
+	}, pcol)
 }
 
 func FilterAndMultiply(numbers []int) []int {
@@ -114,9 +65,5 @@ func FilterAndMultiply(numbers []int) []int {
 }
 
 func SelectNumber(number int) int {
-	if math.Mod(float64(number), 2) == 0 {
-		return number
-	}
-	return 0
-
+	return number % 2
 }
